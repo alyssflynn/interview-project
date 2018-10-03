890b2e7d47e4a46cbb2378f4dbc8b85f4be33c88
Alyss Flynn <alyss.flynn@me.com>
2018-10-03 11:20:28 -0500

    added scraper endpoint

--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@ require 'rubygems'
 require 'bundler/setup'
 require 'sinatra'
 require 'json'
+require 'httparty'
+require 'nokogiri'
 
 set :bind, '0.0.0.0'
 set :port, '8080'
@@ -11,4 +13,16 @@ get '/' do
   env_string = JSON.pretty_generate(ENV.to_a).gsub!("\n",'</br>')
   output += "Environment: </br> #{env_string} </br>"
   output
+end
+
+get '/section/:section_number' do
+  response = HTTParty.get('https://www.osha.gov/Publications/OSHA3514.html')
+  @doc = Nokogiri::HTML(response)
+  sections = @doc.css('div.well.well-small.well-white')
+  output = "#{sections[params[:section_number].to_i]}"
+  output
+end
+
+get '/hello' do
+  "Hello World"
 end
